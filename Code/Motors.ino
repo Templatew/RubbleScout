0a4c9da9f416984a43c28677a4adc61518f2f405
Cazaubon Lorenz <96289463+Templatew@users.noreply.github.com>
2024-01-21 00:41:14 +0100

    push

--- Code/Motors.ino
+++ Code/Motors.ino
@@ -1,13 +1,11 @@
+#include<SoftwareSerial.h>
+
 #define DIR1 7
 #define PW1 6
 #define DIR2 4
 #define PW2 5
 
-int speed_left = 0;
-int speed_right = 0;
 
-
-#include<SoftwareSerial.h>
 #define RX 11
 #define TX 10
 SoftwareSerial BlueT(RX,TX);
@@ -37,12 +35,11 @@ void move(int speed_left, int speed_right) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
   pinMode(DIR1,OUTPUT);
   pinMode(DIR2,OUTPUT);
-  // pinMode(PW1,OUTPUT);
-  // pinMode(PW2,OUTPUT);
+
   digitalWrite(DIR1,LOW);
   digitalWrite(DIR2,LOW);
 
@@ -53,21 +50,30 @@ void setup() {
 
 void loop(){
   if (BlueT.available()){
+    
     Data=BlueT.read();
-    if (Data=='F') {
-      move(255, 255);
-    }
-    else if (Data=='B') {
-      move(-255, -255);
-    }
-    else if (Data=='L') {
-      move(-255, 255);
-    }
-    else if (Data=='R') {
-      move(255, -255);
-    }
-    else if (Data=='S') {
-      move(0, 0);
+
+    switch(Data) {
+
+      case 'F':
+        move(255, 255);
+        break;
+      
+      case 'B':
+        move(-255, -255);
+        break;
+
+      case 'L':
+        move(-255, 255);
+        break;
+
+      case 'R':
+        move(255, -255);
+        break;
+
+      case 'S':
+        move(0, 0);
+        break;
     }
   }
 }
