b5ddef253015c86a8ea0f5365920e9d6db649d03
Cazaubon Lorenz <96289463+Templatew@users.noreply.github.com>
2024-01-26 10:01:29 +0100

    push

--- Code/Lidar.ino
+++ Code/Lidar.ino
@@ -43,75 +43,79 @@ ____/\\\\\\\\\______________________/\\\__________/\\\__________/\\\\\\_________
 
 LIDARLite myLidarLite;
 
-void setup(){
-  Serial.begin(115200); // Initialize serial connection to display distance readings
+// void setup(){
+//   Serial.begin(115200); // Initialize serial connection to display distance readings
 
-  /*
-    begin(int configuration, bool fasti2c, char lidarliteAddress)
+//   /*
+//     begin(int configuration, bool fasti2c, char lidarliteAddress)
 
-    Starts the sensor and I2C.
+//     Starts the sensor and I2C.
 
-    Parameters
-    ----------------------------------------------------------------------------
-    configuration: Default 0. Selects one of several preset configurations.
-    fasti2c: Default 100 kHz. I2C base frequency.
-      If true I2C frequency is set to 400kHz.
-    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
-      operating manual for instructions.
-  */
-  myLidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
+//     Parameters
+//     ----------------------------------------------------------------------------
+//     configuration: Default 0. Selects one of several preset configurations.
+//     fasti2c: Default 100 kHz. I2C base frequency.
+//       If true I2C frequency is set to 400kHz.
+//     lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+//       operating manual for instructions.
+//   */
+//   myLidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
 
-  /*
-    configure(int configuration, char lidarliteAddress)
+//   /*
+//     configure(int configuration, char lidarliteAddress)
 
-    Selects one of several preset configurations.
+//     Selects one of several preset configurations.
 
-    Parameters
-    ----------------------------------------------------------------------------
-    configuration:  Default 0.
-      0: Default mode, balanced performance.
-      1: Short range, high speed. Uses 0x1d maximum acquisition count.
-      2: Default range, higher speed short range. Turns on quick termination
-          detection for faster measurements at short range (with decreased
-          accuracy)
-      3: Maximum range. Uses 0xff maximum acquisition count.
-      4: High sensitivity detection. Overrides default valid measurement detection
-          algorithm, and uses a threshold value for high sensitivity and noise.
-      5: Low sensitivity detection. Overrides default valid measurement detection
-          algorithm, and uses a threshold value for low sensitivity and noise.
-    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
-      operating manual for instructions.
-  */
-  myLidarLite.configure(0); // Change this number to try out alternate configurations
+//     Parameters
+//     ----------------------------------------------------------------------------
+//     configuration:  Default 0.
+//       0: Default mode, balanced performance.
+//       1: Short range, high speed. Uses 0x1d maximum acquisition count.
+//       2: Default range, higher speed short range. Turns on quick termination
+//           detection for faster measurements at short range (with decreased
+//           accuracy)
+//       3: Maximum range. Uses 0xff maximum acquisition count.
+//       4: High sensitivity detection. Overrides default valid measurement detection
+//           algorithm, and uses a threshold value for high sensitivity and noise.
+//       5: Low sensitivity detection. Overrides default valid measurement detection
+//           algorithm, and uses a threshold value for low sensitivity and noise.
+//     lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+//       operating manual for instructions.
+//   */
+//   myLidarLite.configure(0); // Change this number to try out alternate configurations
+// }
+
+// void loop()
+// {
+//   /*
+//     distance(bool biasCorrection, char lidarliteAddress)
+
+//     Take a distance measurement and read the result.
+
+//     Parameters
+//     ----------------------------------------------------------------------------
+//     biasCorrection: Default true. Take aquisition with receiver bias
+//       correction. If set to false measurements will be faster. Receiver bias
+//       correction must be performed periodically. (e.g. 1 out of every 100
+//       readings).
+//     lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+//       operating manual for instructions.
+//   */
+
+//   // Take a measurement with receiver bias correction and print to serial terminal
+//   Serial.println(myLidarLite.distance());
+
+//   // Take 99 measurements without receiver bias correction and print to serial terminal
+//   for(int i = 0; i < 99; i++)
+//   {
+//     Serial.println(myLidarLite.distance(false));
+//   }
+// }
+
+unsigned long int get_distance_lidar(){
+    unsigned long int distance = myLidarLite.distance();
+    return distance;
 }
-
-void loop()
-{
-  /*
-    distance(bool biasCorrection, char lidarliteAddress)
-
-    Take a distance measurement and read the result.
-
-    Parameters
-    ----------------------------------------------------------------------------
-    biasCorrection: Default true. Take aquisition with receiver bias
-      correction. If set to false measurements will be faster. Receiver bias
-      correction must be performed periodically. (e.g. 1 out of every 100
-      readings).
-    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
-      operating manual for instructions.
-  */
-
-  // Take a measurement with receiver bias correction and print to serial terminal
-  Serial.println(myLidarLite.distance());
-
-  // Take 99 measurements without receiver bias correction and print to serial terminal
-  for(int i = 0; i < 99; i++)
-  {
-    Serial.println(myLidarLite.distance(false));
-  }
-}
-
 
 void scan(){
     // fghjk
