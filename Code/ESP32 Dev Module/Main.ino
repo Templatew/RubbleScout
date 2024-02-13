44c5eb6ec5c3b99aad8b70921e4d4d07eb716736
Cazaubon Lorenz <96289463+Templatew@users.noreply.github.com>
2024-02-13 19:15:21 +0100

    push

--- Code/ESP32 Dev Module/Main.ino
+++ Code/ESP32 Dev Module/Main.ino
@@ -29,6 +29,15 @@ ____/\\\\\\\\\______________________/\\\__________/\\\__________/\\\\\\_________
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include "BluetoothSerial.h"
+#include <Wire.h>
+#include <Servo.h>
+#include <LIDARLite.h>
+#include <SPI.h>
+// #include <SD.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // H-Bridge
 
 #define DIR1 21  // GPIO pin for the right motor direction
@@ -36,10 +45,20 @@ ____/\\\\\\\\\______________________/\\\__________/\\\__________/\\\\\\_________
 #define PW1 19  // GPIO pin for the right motor speed
 #define PW2 5  // GPIO pin for the left motor speed
 
-#define LEDC_CHANNEL_0 0
-#define LEDC_CHANNEL_1 1
-#define LEDC_TIMER_8_BIT 8
-#define LEDC_BASE_FREQ 5000
+#define LEDC_CHANNEL_0 0 // PWM channel for the right motor
+#define LEDC_CHANNEL_1 1 // PWM channel for the left motor
+#define LEDC_TIMER_8_BIT 8 // PWM resolution
+#define LEDC_BASE_FREQ 5000 
+
+
+void setupHbridge() {
+    pinMode(DIR1, OUTPUT);
+    pinMode(DIR2, OUTPUT);
+    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_8_BIT);
+    ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_8_BIT);
+    ledcAttachPin(PW1, LEDC_CHANNEL_0);
+    ledcAttachPin(PW2, LEDC_CHANNEL_1);
+}
 
 /**
  * @brief Set the direction and speed of the motors.
@@ -70,14 +89,23 @@ void move(int speedLeft, int speedRight) {
     ledcWrite(LEDC_CHANNEL_1, abs(speedLeft));
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Bluetooth
-#include "BluetoothSerial.h"
 #define USE_PIN
 const char *pin = "1234"; // Change this to a more secure PIN.
 
 String device_name = "ESP32-BT-Slave";
+
+void setupBluetooth() {
+    SerialBT.begin(device_name); // Bluetooth device name
+    // Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+    #ifdef USE_PIN
+        SerialBT.setPin(pin);
+        // Serial.println("Using PIN");
+    #endif
+}
 
 /**
  * @brief Process a command received via Bluetooth.
@@ -112,29 +140,67 @@ void processCommand(String command) {
     }
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Servo
+
+Servo servo; // Create a servo object to control the pan-tilt mechanism
+#define ANGLE_MIN 1000 // Minimum angle for servo in microseconds (0 degrees = 500 microseconds)
+#define ANGLE_MAX 1600 // Maximum angle for servo in microseconds (180 degrees = 2500 microseconds)
+#define ANGLE_MOY (ANGLE_MAX + ANGLE_MIN) / 2 // Middle angle for servo in microseconds
+
+#define ANGLE_MIN_DEG map(ANGLE_MIN, 500, 2500, 0, 180) // Minimum angle for servo in degrees
+#define ANGLE_MAX_DEG map(ANGLE_MAX, 500, 2500, 0, 180) // Maximum angle for servo in degrees
+
+#define RANGE_MICROSECONDS ANGLE_MAX - ANGLE_MIN // Range of servo in microseconds
+#define RANGE_DEGREES ANGLE_MAX_DEG - ANGLE_MIN_DEG // Range of servo in degrees
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Stepper motor
+const double STEPS_ANGLE_DEFAULT = 1.8; // Number of degrees per step in default mode
+int microsteps = 16; // Number of microsteps per step
+double stepsAngle = STEPS_ANGLE_DEFAULT / microsteps; // Number of degrees per step
+int stepsPerRev = 360 / stepsAngle; // Number of steps per revolution
+#define MAX_DELAY 2000 // Maximum delay between steps in microseconds
+#define MIN_DELAY 500 // Minimum delay between steps in microseconds
+int delayStepperMotor = 500; // Delay between steps in microseconds
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// IR sensor
+#define IR_THRESHOLD 90 // Threshold for IR sensor
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// SD card
+#define FILENAME "data.txt" // Name of file to save data to
+File dataFile; // File to save data to
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Lidar Lite v3
+LIDARLite myLidarLite;
+double cartesian[3]; // Cartesian coordinates
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
     Serial.begin(115200);
 
-    // Bluetooth
-    SerialBT.begin(device_name); // Bluetooth device name
-    Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-    #ifdef USE_PIN
-        SerialBT.setPin(pin);
-        Serial.println("Using PIN");
-    #endif
+    // // Bluetooth
+    // SerialBT.begin(device_name); // Bluetooth device name
+    // Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+    // #ifdef USE_PIN
+    //     SerialBT.setPin(pin);
+    //     Serial.println("Using PIN");
+    // #endif
 
     // H-Bridge
-    pinMode(DIR1, OUTPUT);
-    pinMode(DIR2, OUTPUT);
+    setupHbridge();
 
-    // PWM Setup
-    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_8_BIT);
-    ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_8_BIT);
-    // Attach the channel to the GPIO to be controlled
-    ledcAttachPin(PW1, LEDC_CHANNEL_0);
-    ledcAttachPin(PW2, LEDC_CHANNEL_1);
+    // Bluetooth
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
