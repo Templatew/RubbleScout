deec107d0e487081079fde7f972f0a9718cbbdfb
Cazaubon Lorenz <96289463+Templatew@users.noreply.github.com>
2024-04-16 22:41:30 +0200

    push

--- Code/Jetson Nano Nvidia/Fingers Recognition/Arduino/main.ino
+++ Code/Jetson Nano Nvidia/Fingers Recognition/Arduino/main.ino
@@ -10,26 +10,57 @@ ____/\\\\\\\\\______________________/\\\__________/\\\__________/\\\\\\_________
         _\///________\///____\/////////____\/////////____\/////////____\/////////_____\//////////_____\///////////_________\////////______\/////_______\/////////________\/////____
 */
 #include <Wire.h>
-
-const int led_pin_1 =
+// Write in binary the nummber on the leds
+const int led_pin_1 = 3;
+const int led_pin_2 = 4;
+const int led_pin_3 = 5;
 
 void setup() {
-  Wire.begin(0x55);              // join i2c bus with address #4
-  Wire.onReceive(receiveEvent);  // register event
-  Serial.begin(115200);            // start serial for output
+    Wire.begin(0x55);              // join i2c bus with address #4
+    Wire.onReceive(receiveEvent);  // register event
+    Serial.begin(9600);            // start serial for output
+    pinMode(led_pin_1, OUTPUT);
+    pinMode(led_pin_2, OUTPUT);
+    pinMode(led_pin_3, OUTPUT);
 }
 
 void loop() {
-  delay(100);
+    delay(100);
 }
 
 void receiveEvent(int howMany) {
-  while (1 < Wire.available())  // loop through all but the last
-  {
+    while (1 < Wire.available())  // loop through all but the last
+    {
     char c = Wire.read();  // receive byte as a character
     Serial.print(c);       // print the character
-  }
-  int n = Wire.read();  // receive byte as an integer
-  Serial.println(n);    // print the integer
-
+    }
+    int n = Wire.read();  // receive byte as an integer
+    Serial.println(n);    // print the integer
+    // Write the number in binary on the leds
+    // n from 0 to 5
+    if (n == 0) {
+      digitalWrite(led_pin_1, LOW);
+      digitalWrite(led_pin_2, LOW);
+      digitalWrite(led_pin_3, LOW);
+    } else if (n == 1) {
+      digitalWrite(led_pin_1, HIGH);
+      digitalWrite(led_pin_2, LOW);
+      digitalWrite(led_pin_3, LOW);
+    } else if (n == 2) {
+      digitalWrite(led_pin_1, LOW);
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, LOW);
+    } else if (n == 3) {
+      digitalWrite(led_pin_1, HIGH);
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, LOW);
+    } else if (n == 4) {
+      digitalWrite(led_pin_1, LOW);
+      digitalWrite(led_pin_2, LOW);
+      digitalWrite(led_pin_3, HIGH);
+    } else if (n == 5) {
+      digitalWrite(led_pin_1, HIGH);
+      digitalWrite(led_pin_2, LOW);
+      digitalWrite(led_pin_3, HIGH);
+    }
 }
